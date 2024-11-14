29a69c0e6515e8e437ab21a6b5093778bd445582
Miguel Vicente <164623+mvm@users.noreply.github.com>
2024-11-14 10:17:39 +0100

    Initial commit

---
+++ src/main/java/com/mvmoure/swttemplate/App.java
@@ -0,0 +1,13 @@
+package com.mvmoure.swttemplate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
